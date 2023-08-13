f90acb5639cabf1ee68247c22fb5158418e0013b
Mohammed Muti <mohammedmuti74@gamil.com>
2023-08-14 00:29:33 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Home from "./Components/pages/Home/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./Components/pages/Contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home /> */}
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
