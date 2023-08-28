27543ead24ecd4df716dc8e71ae7dc0a2fbd0c61
Mohammed Muti <mohammedmuti74@gamil.com>
2023-08-28 16:02:00 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Thank from "./Components/pages/Thankyou/Thank";
 import WhyUs from "./Components/pages/WhyUs/WhyUs";
 import Services from "./Components/pages/Services/Services";
 import CarpetService from "./Components/pages/Services/CarpetService";
+import BlogPageComp from "./Components/pages/Blog/BlogPageComp";
 
 function App() {
   return (
@@ -15,10 +16,8 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
-
           {/* Contact Us */}
           <Route exact path="/contact-us" element={<Contact />} />
-
           {/* Services */}
           <Route exact path="/services" element={<Services />} />
           {/* Carpet Service */}
@@ -33,60 +32,64 @@ function App() {
             path="/services/rug-cleaning-melbourne"
             element={<Services />}
           />
-
           {/* Thank You */}
           <Route exact path="/thank-you" element={<Thank />} />
-
           {/* Why Us */}
           <Route exact path="/why-us" element={<WhyUs />} />
-
           {/* Blog  */}
           <Route exact path="/blog" element={<Blog />} />
-          <Route exact path="/sheep-skin-rug-cleaning" element={<Blog />} />
           <Route
             exact
-            path="/how-to-use-baking-soda-for-rug-cleaning"
-            element={<Blog />}
+            path="/blog/sheep-skin-rug-cleaning"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/rug-cleaning-vs-carpet-cleaning"
-            element={<Blog />}
+            path="/blog/how-to-use-baking-soda-for-rug-cleaning"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/why-do-I-need-a-professional-rug-cleaner"
-            element={<Blog />}
+            path="/blog/rug-cleaning-vs-carpet-cleaning"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/tips-for-cleaning-cowhide-rug"
-            element={<Blog />}
+            path="/blog/why-do-I-need-a-professional-rug-cleaner"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/how-to-wash-rugs-for-best-results"
-            element={<Blog />}
+            path="/blog/tips-for-cleaning-cowhide-rug"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/why-you-should-prefer-carpet-steam-cleaning-over-diy-cleaning-methods"
-            element={<Blog />}
+            path="/blog/how-to-wash-rugs-for-best-results"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/why-do-you-need-rug-cleaning-experts"
-            element={<Blog />}
+            path="/blog/why-you-should-prefer-carpet-steam-cleaning-over-diy-cleaning-methods"
+            element={<BlogPageComp />}
           />
           <Route
             exact
-            path="/what-is-best-for-you-rug-steam-dry-or-traditional-hand-cleaning"
-            element={<Blog />}
+            path="/blog/why-do-you-need-rug-cleaning-experts"
+            element={<BlogPageComp />}
           />
-          <Route exact path="/how-to-clean-a-rug?" element={<Blog />} />
+          <Route
+            exact
+            path="/blog/what-is-best-for-you-rug-steam-dry-or-traditional-hand-cleaning"
+            element={<BlogPageComp />}
+          />
 
+          <Route
+            exact
+            path="/blog/how-to-clean-a-rug"
+            element={<BlogPageComp />}
+          />
           <Route exact path="/faq" element={<FAQ />} />
-
           {/* Terms and Conditions */}
           <Route exact path="/terms-and-conditions" element={<Terms />} />
         </Routes>
--- src/Components/pages/Blog/Blog.jsx
+++ src/Components/pages/Blog/Blog.jsx
@@ -12,46 +12,63 @@ const Blog = () => {
       <Banner sublink="Blog" />
 
       <div className="blogs">
+        <div className="blog heading">
+          <h2>Blogs</h2>
+        </div>
         <div className="blog content">
           <BlogComp
             title="Sheep Skin Rug Cleaning"
             img={require("../../../Assets/Images/Blogs/Sheep.png")}
+            link={"/blog/sheep-skin-rug-cleaning"}
           />
           <BlogComp
             title="How to use baking soda for rug cleaning"
             img={require("../../../Assets/Images/Blogs/soda.jpg")}
+            link={"/blog/how-to-use-baking-soda-for-rug-cleaning"}
           />
           <BlogComp
             title="Rug cleaning vs Carpet cleaning"
             img={require("../../../Assets/Images/Blogs/versus.jpg")}
+            link={"/blog/rug-cleaning-vs-carpet-cleaning"}
           />
           <BlogComp
             title="Why do I need a professional rug cleaner?"
             img={require("../../../Assets/Images/Blogs/professional.jpg")}
+            link={"/blog/why-do-I-need-a-professional-rug-cleaner"}
           />
           <BlogComp
             title="TIPS for cleaning cowhide Rug"
             img={require("../../../Assets/Images/Blogs/tips.jpg")}
+            link={"/blog/tips-for-cleaning-cowhide-rug"}
           />
           <BlogComp
             title="How to Wash Rugs for Best Results?"
             img={require("../../../Assets/Images/Blogs/wash.jpg")}
+            link={"/blog/how-to-wash-rugs-for-best-results"}
           />
           <BlogComp
             title="Why You Should Prefer Carpet Steam Cleaning Over DIY Cleaning Methods?"
             img={require("../../../Assets/Images/Blogs/steam.jpg")}
+            link={
+              "/blog/why-you-should-prefer-carpet-steam-cleaning-over-diy-cleaning-methods"
+            }
           />
           <BlogComp
             title="Why Do You Need Rug Cleaning Experts?"
             img={require("../../../Assets/Images/Blogs/experts.jpg")}
+            link={"/blog/why-do-you-need-rug-cleaning-experts"}
           />
           <BlogComp
             title="What is Best for You? Rug Steam, Dry or Traditional Hand Cleaning?"
             img={require("../../../Assets/Images/Blogs/traditional.jpg")}
+            link={
+              "/blog/what-is-best-for-you-rug-steam-dry-or-traditional-hand-cleaning"
+            }
           />
           <BlogComp
             title="How to Clean a Rug?"
             img={require("../../../Assets/Images/Blogs/clean.jpg")}
+            link={"/blog/how-to-clean-a-rug?"}
           />
         </div>
       </div>
