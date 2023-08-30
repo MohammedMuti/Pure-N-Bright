82eedd3a9721d066a7c7d8630eb3cb3a835fa7b7
Mohammed Muti <mohammedmuti74@gamil.com>
2023-08-30 16:48:31 +0530

    commit

--- src/Components/pages/Gallery/RugGallery.jsx
+++ src/Components/pages/Gallery/RugGallery.jsx
@@ -9,7 +9,101 @@ const RugGallery = () => {
     <>
       <Navbar />
       <Banner sublink="Gallery" sublink2="Rug Cleaning Gallery" />
-      <GallerySubComp title="Rug" />
+      <GallerySubComp
+        title="Rug"
+        images={[
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/1.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/2.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/3.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/4.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/5.webp"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/6.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/7.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/8.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/9.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/10.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/11.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/12.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/13.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/14.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/15.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/16.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/17.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/18.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/19.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/20.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/21.jpeg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/22.jpeg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/23.jpeg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/24.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/25.jpeg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/26.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/27.jpg"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/28.png"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/29.png"),
+          },
+          {
+            src: require("../../../Assets/Images/Gallery/Rug/30.jpeg"),
+          },
+        ]}
+      />
       <ContactComp />
       <Footer />
     </>
