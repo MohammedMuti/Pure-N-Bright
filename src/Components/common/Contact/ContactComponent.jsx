515797811b50df4953fc84a8383538263e33446c
Mohammed Muti <mohammedmuti74@gamil.com>
2023-08-31 18:08:09 +0530

    commit

--- src/Components/common/Contact/ContactComponent.jsx
+++ src/Components/common/Contact/ContactComponent.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./Contact.css";
 import "../../pages/Home/Home.css";
+import schema from "./validate";
+import ReCAPTCHA from "react-google-recaptcha";
+import axios from "axios";
 
 const ContactComp = () => {
   const [fullNameError, setFullNameError] = useState("");
@@ -36,87 +39,45 @@ const ContactComp = () => {
       });
     }
 
-    // if (validation === true) {
-    //   const responsetoself = await axios(
-    //     "https://nodemailer-web.onrender.com/sendToUser",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-type": "application/json",
-    //       },
-    //       data: mailerState,
-    //     }
-    //   )
-    //     .then((res) => {
-    //       console.log(res);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    //   const responseToAsif = await axios(
-    //     "https://nodemailer-web.onrender.com/sendToAsif",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-type": "application/json",
-    //       },
-    //       data: mailerState,
-    //     }
-    //   )
-    //     .then((res) => {
-    //       console.log(res);
-    //       if (res.status === 200) {
-    //         console.log("Message Sent");
-    //       } else if (res.status === 402) {
-    //         console.log("Message failed to send");
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    //   const responseToAbuzar = await axios(
-    //     "https://nodemailer-web.onrender.com/sendToAbuzar",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-type": "application/json",
-    //       },
-    //       data: mailerState,
-    //     }
-    //   )
-    //     .then((res) => {
-    //       console.log(res);
-    //       if (res.status === 200) {
-    //         console.log("Message Sent");
-    //       } else if (res.status === 402) {
-    //         console.log("Message failed to send");
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    //   const responseToMuti = await axios(
-    //     "https://nodemailer-web.onrender.com/sendToMuti",
-    //     {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-type": "application/json",
-    //       },
-    //       data: mailerState,
-    //     }
-    //   )
-    //     .then((res) => {
-    //       console.log(res);
-    //       if (res.status === 200) {
-    //         console.log("Message Sent");
-    //       } else if (res.status === 402) {
-    //         console.log("Message failed to send");
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       console.log(err);
-    //     });
-    // }
+    if (validation === true) {
+      const responsetoself = await axios(
+        "https://purenbright-backend.onrender.com/sendToUser",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          data: mailerState,
+        }
+      )
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      const responseToMuti = await axios(
+        "https://purenbright-backend.onrender.com/sendToMuti",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          data: mailerState,
+        }
+      )
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            console.log("Message Sent");
+          } else if (res.status === 402) {
+            console.log("Message failed to send");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   function handleStateChange(e) {
@@ -127,67 +88,67 @@ const ContactComp = () => {
   }
 
   const validate = (mailerState) => {
-    // const result = schema.validate(
-    //   {
-    //     fullName: mailerState.fullName,
-    //     email: mailerState.email,
-    //     number: mailerState.phone,
-    //     message: mailerState.message,
-    //   },
-    //   { abortEarly: false }
-    // );
-    // if (!result.error) {
-    //   setFullNameError("");
-    //   setPhoneError("");
-    //   setMessageError("");
-    //   setEmailError("");
-    //   setErrorMessage(false);
-    //   return true;
-    // }
-    // for (let i in result.error.details) {
-    //   if (result.error.details[i].context.label === "Name") {
-    //     setFullNameError(result.error.details[i].message);
-    //     setErrorMessage(true);
-    //     break;
-    //   } else setFullNameError("");
-    //   setErrorMessage(false);
-    // }
-    // for (let i in result.error.details) {
-    //   if (result.error.details[i].context.label === "Email") {
-    //     setEmailError(result.error.details[i].message);
-    //     setErrorMessage(true);
-    //     break;
-    //   } else setEmailError("");
-    //   setErrorMessage(false);
-    // }
-    // for (let i in result.error.details) {
-    //   if (result.error.details[i].context.label === "Number") {
-    //     setPhoneError(result.error.details[i].message);
-    //     setErrorMessage(true);
-    //     break;
-    //   } else setPhoneError("");
-    //   setErrorMessage(false);
-    // }
-    // for (let i in result.error.details) {
-    //   if (result.error) {
-    //     if (result.error.details[i].context.label === "Message") {
-    //       setMessageError(result.error.details[i].message);
-    //       setErrorMessage(true);
-    //       break;
-    //     }
-    //   } else setMessageError("");
-    //   setErrorMessage(false);
-    // }
-    // if (!result.error) {
-    //   setFullNameError("");
-    //   setPhoneError("");
-    //   setMessageError("");
-    //   setEmailError("");
-    //   setErrorMessage(false);
-    //   return true;
-    // } else {
-    //   return false;
-    // }
+    const result = schema.validate(
+      {
+        fullName: mailerState.fullName,
+        email: mailerState.email,
+        number: mailerState.phone,
+        message: mailerState.message,
+      },
+      { abortEarly: false }
+    );
+    if (!result.error) {
+      setFullNameError("");
+      setPhoneError("");
+      setMessageError("");
+      setEmailError("");
+      setErrorMessage(false);
+      return true;
+    }
+    for (let i in result.error.details) {
+      if (result.error.details[i].context.label === "Name") {
+        setFullNameError(result.error.details[i].message);
+        setErrorMessage(true);
+        break;
+      } else setFullNameError("");
+      setErrorMessage(false);
+    }
+    for (let i in result.error.details) {
+      if (result.error.details[i].context.label === "Email") {
+        setEmailError(result.error.details[i].message);
+        setErrorMessage(true);
+        break;
+      } else setEmailError("");
+      setErrorMessage(false);
+    }
+    for (let i in result.error.details) {
+      if (result.error.details[i].context.label === "Number") {
+        setPhoneError(result.error.details[i].message);
+        setErrorMessage(true);
+        break;
+      } else setPhoneError("");
+      setErrorMessage(false);
+    }
+    for (let i in result.error.details) {
+      if (result.error) {
+        if (result.error.details[i].context.label === "Message") {
+          setMessageError(result.error.details[i].message);
+          setErrorMessage(true);
+          break;
+        }
+      } else setMessageError("");
+      setErrorMessage(false);
+    }
+    if (!result.error) {
+      setFullNameError("");
+      setPhoneError("");
+      setMessageError("");
+      setEmailError("");
+      setErrorMessage(false);
+      return true;
+    } else {
+      return false;
+    }
   };
 
   function onChange(value) {
@@ -267,10 +228,10 @@ const ContactComp = () => {
                     style={{ transformOrigin: "0 0", width: "100%" }}
                     className="captcha"
                   >
-                    {/* <ReCAPTCHA
-                sitekey="6LePrU4kAAAAAJtpRmNFy9i-u7PNdMnjp-PIeAsP"
-                onChange={onChange}
-              /> */}
+                    <ReCAPTCHA
+                      sitekey="6LePrU4kAAAAAJtpRmNFy9i-u7PNdMnjp-PIeAsP"
+                      onChange={onChange}
+                    />
                   </div>
 
                   <input type="submit" value="send message" className="btn" />
